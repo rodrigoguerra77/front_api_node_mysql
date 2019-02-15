954654a02485aa8dee5c4270d56f1c39214f1d88
Rodrigo Guerra <rodrigoguerra77@gmail.com>
2019-02-15 16:56:28 -0400

    Creado una página de inicio

---
+++ src/components/Index.vue
@@ -0,0 +1,33 @@
+<template lang="html">
+  <div class="index">
+    <b-jumbotron class="bg-vue" text-variant="white" border-variant="dark">
+      <template slot="header">Welcome to B-V Courses</template>
+
+      <template slot="lead">
+        The best place to learn, we have millions of online courses on web development.
+      </template>
+
+      <hr class="my-4" />
+
+      <p>
+        You will find everything you need to be a web developer.
+      </p>
+    </b-jumbotron>
+  </div>
+</template>
+
+<script>
+  export default {
+    name : 'index'
+  }
+</script>
+
+<style lang="css" scoped>
+  .index {
+    padding: 25px;
+  }
+
+  .bg-vue {
+    background-color: #42b983;
+  }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,11 +2,17 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Courses from '@/components/Courses.vue';
 import Students from '@/components/Students.vue';
+import Index from '@/components/Index.vue';
 
 Vue.use(Router);
 
 export default new Router({
     routes: [
+      {
+          path: '/',
+          name: 'index',
+          component: Index
+      },
       {
           path: '/courses',
           name: 'courses',

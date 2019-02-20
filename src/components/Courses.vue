8ef8d910fd76b2e7712c8fee8bd20238497e66a5
Rodrigo Guerra <rodrigoguerra77@gmail.com>
2019-02-20 18:16:38 -0400

    Separado en un componente el formulario de registro de cursos

--- src/components/Courses.vue
+++ src/components/Courses.vue
@@ -5,25 +5,7 @@
     <b-container fluid>
       <!-- User Interface controls -->
       <b-row>
-        <b-col md="6" class="my-1">
-          <b-button class="btn-send" v-b-modal.modalPrevent>Create Course</b-button>
-          <b-modal
-            id="modalPrevent"
-            ref="modal"
-            title="Create Course"
-            @ok="handleOk"
-          >
-            <form @submit.stop.prevent="handleSubmit">
-              <!--<b-form-input type="text" v-model="newItem.courses_id" disabled/>-->
-              <label for="name">Name</label><b-form-input type="text" placeholder="Enter the course name" v-model="newItem.courses_name" />
-              <label for="description">Description</label><b-form-input type="text" placeholder="Enter the course description" v-model="newItem.courses_descrption" />
-              <label for="start">Start date</label><b-form-input type="date" v-model="newItem.courses_start_date" />
-              <label for="end">End date</label><b-form-input type="date" v-model="newItem.courses_final_date" />
-              <!--<b-form-input type="text" v-model="newItem.status_id" disabled/>-->
-              <label for="students">Students</label><b-form-input type="number" v-model="newItem.courses_students" />
-            </form>
-          </b-modal>
-        </b-col>
+        <create-courses></create-courses>
         <b-col md="6" class="my-1">
           <b-form-group label-cols-sm="3" label="" class="mb-0">
             <b-input-group>
@@ -79,19 +61,18 @@
 
 <script>
   import http from "../http-common";
+  import CreateCourses from "./CreateCourses.vue";
 
   const items = [];
 
   export default {
+    components: {
+      CreateCourses
+    },
     name: 'courses',
     data() {
       return {
         items: items,
-        submitted: false,
-        newItem : {
-          courses_id : 0,
-          status_id : 1
-        },
         fields: [
           { key: 'courses_name', label: 'Name', sortDirection: 'desc' },
           { key: 'courses_descrption', label: 'Description' },
@@ -127,42 +108,6 @@
         this.totalRows = filteredItems.length
         this.currentPage = 1
       },
-      handleOk(evt) {
-        // Prevent modal from closing
-        evt.preventDefault()
-        //if (!this.name) {
-        //  alert('Please enter your name')
-        //} else {
-          this.handleSubmit()
-        //}
-      },
-      handleSubmit() {
-        var data = {
-          courses_id: 0,
-          courses_name: this.newItem.courses_name,
-          courses_descrption: this.newItem.courses_descrption,
-          courses_descrption: this.newItem.courses_descrption,
-          courses_start_date: this.newItem.courses_start_date,
-          courses_final_date: this.newItem.courses_final_date,
-          courses_students: this.newItem.courses_students,
-          status_id: 1,
-        };
-        http
-        .post("/", data)
-        .then(response => {
-          this.items.courses_id = response.data.courses_id;
-          console.log(response.data);
-        })
-        .catch(e => {
-          console.log(e);
-        });
-
-        this.submitted = true;
-        //this.items.push(this.newItem)
-        //this.newItem = {}
-        this.$refs.modal.hide()
-
-      },
       retrieveCourses() {
       http
         .get("/")
@@ -189,12 +134,6 @@
     color: #42b983;
     font-weight: bold;
     padding: 15px;
-  }
-
-  .btn-send {
-    color: #fff;
-    background-color: #42b983;
-    border-color: #42b983;
   }
 
 </style>
---
+++ src/components/CreateCourses.vue
@@ -0,0 +1,82 @@
+<template lang="html">
+  <b-col md="6" class="my-1">
+    <b-button class="btn-send" v-b-modal.modalPrevent>Create Course</b-button>
+    <b-modal
+      id="modalPrevent"
+      ref="modal"
+      title="Create Course"
+      @ok="handleOk"
+    >
+      <form @submit.stop.prevent="handleSubmit">
+        <label for="name">Name</label><b-form-input type="text" placeholder="Enter the course name" v-model="newItem.courses_name" />
+        <label for="description">Description</label><b-form-input type="text" placeholder="Enter the course description" v-model="newItem.courses_descrption" />
+        <label for="start">Start date</label><b-form-input type="date" v-model="newItem.courses_start_date" />
+        <label for="end">End date</label><b-form-input type="date" v-model="newItem.courses_final_date" />
+        <label for="students">Students</label><b-form-input type="number" v-model="newItem.courses_students" />
+      </form>
+    </b-modal>
+  </b-col>
+</template>
+
+<script>
+  import http from "../http-common";
+
+  export default {
+    name: 'create-courses',
+    data() {
+      return {
+        submitted: false,
+        newItem : {
+          courses_id : 0,
+          status_id : 1
+        },
+      }
+    },
+    methods: {
+      handleOk(evt) {
+        // Prevent modal from closing
+        evt.preventDefault()
+        //if (!this.name) {
+        //  alert('Please enter your name')
+        //} else {
+          this.handleSubmit()
+        //}
+      },
+      handleSubmit() {
+        var data = {
+          courses_id: 0,
+          courses_name: this.newItem.courses_name,
+          courses_descrption: this.newItem.courses_descrption,
+          courses_descrption: this.newItem.courses_descrption,
+          courses_start_date: this.newItem.courses_start_date,
+          courses_final_date: this.newItem.courses_final_date,
+          courses_students: this.newItem.courses_students,
+          status_id: 1,
+        };
+        http
+        .post("/", data)
+        .then(response => {
+          this.newItem.courses_id = response.data.courses_id;
+          console.log(response.data);
+        })
+        .catch(e => {
+          console.log(e);
+        });
+
+        this.submitted = true;
+        //this.items.push(this.newItem)
+        //this.newItem = {}
+        this.$refs.modal.hide()
+
+      }
+    }
+  }
+</script>
+
+<style lang="css" scoped>
+  .btn-send {
+    color: #fff;
+    background-color: #42b983;
+    border-color: #42b983;
+  }
+</style>
